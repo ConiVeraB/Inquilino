be1ad51fc5c4dc74420c265a31dc0f45b31051ca
ConiVeraB <C.verabustillos@uandresbello.edu>
2025-05-20 22:34:45 -0400

    PP + player + estetica
    -Re ajuste de Post processing (Zona clara  y efecto desenfoque)
    -Aumento de velocidad del jugador
    -Skybox nocturno

--- Encargo N°1 - Constanza Vera/Assets/Scripts/SetNavigationStatic.cs
+++ Encargo N°1 - Constanza Vera/Assets/Scripts/SetNavigationStatic.cs
@@ -6,8 +6,9 @@ public class SetNavigationStatic : MonoBehaviour
     [ContextMenu("Marcar como Navigation Static")]
     void MarcarStatic()
     {
+        
         GameObject obj = this.gameObject;
-        GameObjectUtility.SetStaticEditorFlags(obj, StaticEditorFlags.NavigationStatic);
+       // GameObjectUtility.SetStaticEditorFlags(obj, StaticEditorFlags.NavigationStatic);
         Debug.Log($"{obj.name} marcado como Navigation Static.");
     }
 }
