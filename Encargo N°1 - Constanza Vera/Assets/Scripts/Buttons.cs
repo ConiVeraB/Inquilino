d01e2b1ea86d9bfd43aea65412fa5ea354473572
ConiVeraB <C.verabustillos@uandresbello.edu>
2025-05-22 23:03:23 -0400

    Feedback aplicado a cambios
    MENU
    -Cursor limitado a sólo el juego
    -Botón de créditos arreglado
    -Panel con los créditos
    -Botón "Volver" esconde créditos

--- Encargo N°1 - Constanza Vera/Assets/Scripts/Buttons.cs
+++ Encargo N°1 - Constanza Vera/Assets/Scripts/Buttons.cs
@@ -1,7 +1,25 @@
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 public class Buttons : MonoBehaviour
 {
+    public GameObject creditsPanel;
+
+    void Start()
+    {
+        Cursor.lockState = CursorLockMode.Confined;
+        Cursor.visible = true;
+        if (creditsPanel != null)
+        {
+            creditsPanel.SetActive(false);
+        }
+        else
+        {
+            Debug.LogWarning("El Panel de Créditos no ha sido asignado en el Inspector para el script Buttons.");
+        }
+    }
+
+    
     public void Play()
     {
         SceneManager.LoadScene("Game"); 
@@ -17,6 +35,29 @@ public class Buttons : MonoBehaviour
         SceneManager.LoadScene("Main Menú");
     }
 
+    public void ShowCredits() 
+    {
+        if (creditsPanel != null)
+        {
+            creditsPanel.SetActive(true);
+        }
+        else
+        {
+            Debug.LogError("Por ahi no era");
+        }
+    }
+
+    public void HideCredits()
+    {
+        if (creditsPanel != null)
+        {
+            creditsPanel.SetActive(false);
+        }
+        else
+        {
+            Debug.LogError("Nope");
+        }
+    }
     public void Quit()
     {
         Application.Quit();
