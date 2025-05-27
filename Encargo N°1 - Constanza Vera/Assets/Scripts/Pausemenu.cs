af238b7f46aba0fcd7d1199cb78c2d9bcc2f50c2
ConiVeraB <C.verabustillos@uandresbello.edu>
2025-05-27 00:11:23 -0400

    Feedback Update #1
    -Sistema de diálogo (Aún en implementación)
    -Limpieza de archivos (no usados)
    -Menú de pausa (por modificar)

---
+++ Encargo N°1 - Constanza Vera/Assets/Scripts/Pausemenu.cs
@@ -0,0 +1,32 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Pausemenu : MonoBehaviour
+{
+    [SerializeField] GameObject pauseMenu;
+    public void Pause()
+    {
+        pauseMenu.SetActive(false);
+        Time.timeScale = 0;
+    }
+
+    public void Home()
+    {
+        SceneManager.LoadScene("Main Menú");
+        Time.timeScale = 1;
+    }
+
+    public void Resume()
+    {
+        pauseMenu.SetActive(false);
+        Time.timeScale = 1;
+    }
+
+    public void Restart()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+        Time.timeScale = 1;
+    }
+
+
+}
