5916bc16db6ff2baf87da86717a5b170c607ab85
ConiVeraB <C.verabustillos@uandresbello.edu>
2025-06-10 03:05:18 -0400

    Cursor
    - Cursor
    - Script para cambiarlo cuando se acerque a las puertas
    -Se quitó el texto de "Objetivos" que no debería estar ahi

---
+++ Encargo N°1 - Constanza Vera/Assets/Scripts/Cursor Script/CursorController.cs
@@ -0,0 +1,14 @@
+using UnityEngine;
+
+
+public class CursorController : MonoBehaviour
+{
+    [SerializeField] private Texture2D cursor;
+    private Vector2 cursorHotspot;
+    void Start()
+    {
+        cursorHotspot = new Vector2(cursor.width / 2, cursor.height / 2);
+        Cursor.SetCursor(cursor, cursorHotspot, CursorMode.Auto);
+    }
+
+}
