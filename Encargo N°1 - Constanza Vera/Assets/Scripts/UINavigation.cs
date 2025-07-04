8cb588ba190538b01134686f51df7d226aded467
ConiVeraB <C.verabustillos@uandresbello.edu>
2025-07-04 06:33:40 -0400

    Ahora si

--- Encargo N°1 - Constanza Vera/Assets/Scripts/UINavigation.cs
+++ Encargo N°1 - Constanza Vera/Assets/Scripts/UINavigation.cs
@@ -6,6 +6,15 @@ public class UINavigation : MonoBehaviour
 {
     // Esta función pública la llamaremos desde el botón.
     // Recibe el nombre de la escena que queremos cargar.
+
+    void Start()
+    {
+        // Aseguramos el estado correcto para cualquier escena de menú o final.
+        Time.timeScale = 1f;
+        Cursor.lockState = CursorLockMode.None; // Liberar el cursor.
+        Cursor.visible = true;                  // Hacerlo visible.
+    }
+    // --- FIN DEL MÉTODO A AÑADIR --
     public void LoadSceneByName(string MainMenú)
     {
         // Imprime un mensaje en la consola para saber que funciona.
